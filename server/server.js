006184289fc1a77f04916dce90bd95e773c9524f
Aric Johnson <aric87@comcast.net>
2016-08-05 12:44:57 -0400

    seed file working, initial tempalte structure

--- server/server.js
+++ server/server.js
@@ -11,14 +11,22 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 const session  = require('express-session');
-
+const models = require('./models/models');
+const swig = require('swig');
 // set up our express application
-app.use(morgan('dev')); // log every request to the console
-app.use(cookieParser()); // read cookies (needed for auth)
+
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'})
+
+// setup the logger
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.use(morgan('combined', {stream: accessLogStream}))app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // get information from html forms
 // required for passport
-app.use(session({ secret: 'cmsApp', cookie: { maxAge: 360000 },resave: true, saveUninitialized: true})); // session secret
+app.use(session({ secret: 'cmsApp', cookie: { maxAge: 360000 }, resave: true, saveUninitialized: true})); // session secret
+
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 
@@ -31,4 +39,4 @@ app.listen(port,app_ip_address, (err) => {
     if(err) console.log(err)
     console.log('The magic happens on port ' + port);
 });
-    module.exports = app;
+module.exports = app;
